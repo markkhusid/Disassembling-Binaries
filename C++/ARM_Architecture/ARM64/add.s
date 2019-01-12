f261dc853d34204ae9b4cca14dd631ddeaf30761
Mark Khusid <markkhusid@gmail.com>
2019-01-12 11:52:02 -0600

    added aarch64 C++ add_int

---
+++ C++/ARM_Architecture/ARM64/add.s
@@ -0,0 +1,101 @@
+	.arch armv8-a
+	.file	"add.cpp"
+	.text
+	.section	.rodata
+	.align	3
+	.type	_ZStL19piecewise_construct, %object
+	.size	_ZStL19piecewise_construct, 1
+_ZStL19piecewise_construct:
+	.zero	1
+	.local	_ZStL8__ioinit
+	.comm	_ZStL8__ioinit,1,8
+	.text
+	.align	2
+	.global	main
+	.type	main, %function
+main:
+.LFB1517:
+	.cfi_startproc
+	sub	sp, sp, #16
+	.cfi_def_cfa_offset 16
+	mov	w0, 3
+	str	w0, [sp, 12]
+	mov	w0, 5
+	str	w0, [sp, 8]
+	ldr	w1, [sp, 12]
+	ldr	w0, [sp, 8]
+	add	w0, w1, w0
+	str	w0, [sp, 4]
+	mov	w0, 0
+	add	sp, sp, 16
+	.cfi_def_cfa_offset 0
+	ret
+	.cfi_endproc
+.LFE1517:
+	.size	main, .-main
+	.align	2
+	.type	_Z41__static_initialization_and_destruction_0ii, %function
+_Z41__static_initialization_and_destruction_0ii:
+.LFB1998:
+	.cfi_startproc
+	stp	x29, x30, [sp, -32]!
+	.cfi_def_cfa_offset 32
+	.cfi_offset 29, -32
+	.cfi_offset 30, -24
+	mov	x29, sp
+	str	w0, [sp, 28]
+	str	w1, [sp, 24]
+	ldr	w0, [sp, 28]
+	cmp	w0, 1
+	bne	.L5
+	ldr	w1, [sp, 24]
+	mov	w0, 65535
+	cmp	w1, w0
+	bne	.L5
+	adrp	x0, _ZStL8__ioinit
+	add	x0, x0, :lo12:_ZStL8__ioinit
+	bl	_ZNSt8ios_base4InitC1Ev
+	adrp	x0, __dso_handle
+	add	x2, x0, :lo12:__dso_handle
+	adrp	x0, _ZStL8__ioinit
+	add	x1, x0, :lo12:_ZStL8__ioinit
+	adrp	x0, :got:_ZNSt8ios_base4InitD1Ev
+	ldr	x0, [x0, #:got_lo12:_ZNSt8ios_base4InitD1Ev]
+	bl	__cxa_atexit
+.L5:
+	nop
+	ldp	x29, x30, [sp], 32
+	.cfi_restore 30
+	.cfi_restore 29
+	.cfi_def_cfa_offset 0
+	ret
+	.cfi_endproc
+.LFE1998:
+	.size	_Z41__static_initialization_and_destruction_0ii, .-_Z41__static_initialization_and_destruction_0ii
+	.align	2
+	.type	_GLOBAL__sub_I_main, %function
+_GLOBAL__sub_I_main:
+.LFB1999:
+	.cfi_startproc
+	stp	x29, x30, [sp, -16]!
+	.cfi_def_cfa_offset 16
+	.cfi_offset 29, -16
+	.cfi_offset 30, -8
+	mov	x29, sp
+	mov	w1, 65535
+	mov	w0, 1
+	bl	_Z41__static_initialization_and_destruction_0ii
+	ldp	x29, x30, [sp], 16
+	.cfi_restore 30
+	.cfi_restore 29
+	.cfi_def_cfa_offset 0
+	ret
+	.cfi_endproc
+.LFE1999:
+	.size	_GLOBAL__sub_I_main, .-_GLOBAL__sub_I_main
+	.section	.init_array,"aw"
+	.align	3
+	.xword	_GLOBAL__sub_I_main
+	.hidden	__dso_handle
+	.ident	"GCC: (Debian 8.2.0-13) 8.2.0"
+	.section	.note.GNU-stack,"",@progbits
