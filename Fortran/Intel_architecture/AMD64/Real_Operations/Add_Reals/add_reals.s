bae0d07c11287b693c25a51cd7bbfa53600dc535
Ubuntu <ubuntu@ip-172-31-82-215.ec2.internal>
2019-01-17 03:39:57 +0000

    more updates to add_reals Fortran from ec2

---
+++ Fortran/Intel_architecture/AMD64/Real_Operations/Add_Reals/add_reals.s
@@ -0,0 +1,74 @@
+	.file	"add_reals.f08"
+	.text
+	.type	MAIN__, @function
+MAIN__:
+.LFB0:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	movss	.LC0(%rip), %xmm0
+	movss	%xmm0, -4(%rbp)
+	movss	.LC1(%rip), %xmm0
+	movss	%xmm0, -8(%rbp)
+	movss	-8(%rbp), %xmm0
+	addss	-4(%rbp), %xmm0
+	movss	%xmm0, -12(%rbp)
+	nop
+	popq	%rbp
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE0:
+	.size	MAIN__, .-MAIN__
+	.globl	main
+	.type	main, @function
+main:
+.LFB1:
+	.cfi_startproc
+	pushq	%rbp
+	.cfi_def_cfa_offset 16
+	.cfi_offset 6, -16
+	movq	%rsp, %rbp
+	.cfi_def_cfa_register 6
+	subq	$16, %rsp
+	movl	%edi, -4(%rbp)
+	movq	%rsi, -16(%rbp)
+	movq	-16(%rbp), %rdx
+	movl	-4(%rbp), %eax
+	movq	%rdx, %rsi
+	movl	%eax, %edi
+	call	_gfortran_set_args@PLT
+	leaq	options.0.3504(%rip), %rsi
+	movl	$7, %edi
+	call	_gfortran_set_options@PLT
+	call	MAIN__
+	movl	$0, %eax
+	leave
+	.cfi_def_cfa 7, 8
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	main, .-main
+	.section	.rodata
+	.align 16
+	.type	options.0.3504, @object
+	.size	options.0.3504, 28
+options.0.3504:
+	.long	68
+	.long	1023
+	.long	0
+	.long	1
+	.long	1
+	.long	0
+	.long	31
+	.align 4
+.LC0:
+	.long	1092616192
+	.align 4
+.LC1:
+	.long	1073741824
+	.ident	"GCC: (Ubuntu 7.3.0-27ubuntu1~18.04) 7.3.0"
+	.section	.note.GNU-stack,"",@progbits
