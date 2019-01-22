f0aef30a5ff71d06685a10ea32a8f2adf98283a4
Mark Khusid <markkhusid@protonmail.com>
2019-01-22 02:41:24 +0000

    started i386 Ada from Pentest

---
+++ Ada/Intel_Architecture/i386/add_int.s
@@ -0,0 +1,40 @@
+	.file	"add_int.adb"
+	.text
+	.align 2
+	.globl	_ada_add_int
+	.type	_ada_add_int, @function
+_ada_add_int:
+.LFB1:
+	.cfi_startproc
+	pushl	%ebp
+	.cfi_def_cfa_offset 8
+	.cfi_offset 5, -8
+	movl	%esp, %ebp
+	.cfi_def_cfa_register 5
+	subl	$24, %esp
+	call	__x86.get_pc_thunk.ax
+	addl	$_GLOBAL_OFFSET_TABLE_, %eax
+	movl	$3, -12(%ebp)
+	movl	$5, -16(%ebp)
+	movl	$8, -20(%ebp)
+	nop
+	leave
+	.cfi_restore 5
+	.cfi_def_cfa 4, 4
+	ret
+	.cfi_endproc
+.LFE1:
+	.size	_ada_add_int, .-_ada_add_int
+	.section	.text.__x86.get_pc_thunk.ax,"axG",@progbits,__x86.get_pc_thunk.ax,comdat
+	.globl	__x86.get_pc_thunk.ax
+	.hidden	__x86.get_pc_thunk.ax
+	.type	__x86.get_pc_thunk.ax, @function
+__x86.get_pc_thunk.ax:
+.LFB4:
+	.cfi_startproc
+	movl	(%esp), %eax
+	ret
+	.cfi_endproc
+.LFE4:
+	.ident	"GCC: (Ubuntu 7.3.0-27ubuntu1~18.04) 7.3.0"
+	.section	.note.GNU-stack,"",@progbits
---
+++ Ada/Intel_Architecture/i386/b~add_int.adb
@@ -0,0 +1,153 @@
+pragma Ada_95;
+pragma Warnings (Off);
+pragma Source_File_Name (ada_main, Spec_File_Name => "b~add_int.ads");
+pragma Source_File_Name (ada_main, Body_File_Name => "b~add_int.adb");
+pragma Suppress (Overflow_Check);
+
+package body ada_main is
+
+   E16 : Short_Integer; pragma Import (Ada, E16, "system__soft_links_E");
+   E26 : Short_Integer; pragma Import (Ada, E26, "system__exception_table_E");
+   E28 : Short_Integer; pragma Import (Ada, E28, "system__exceptions_E");
+   E20 : Short_Integer; pragma Import (Ada, E20, "system__secondary_stack_E");
+
+   Local_Priority_Specific_Dispatching : constant String := "";
+   Local_Interrupt_States : constant String := "";
+
+   Is_Elaborated : Boolean := False;
+
+   procedure adafinal is
+      procedure s_stalib_adafinal;
+      pragma Import (C, s_stalib_adafinal, "system__standard_library__adafinal");
+
+      procedure Runtime_Finalize;
+      pragma Import (C, Runtime_Finalize, "__gnat_runtime_finalize");
+
+   begin
+      if not Is_Elaborated then
+         return;
+      end if;
+      Is_Elaborated := False;
+      Runtime_Finalize;
+      s_stalib_adafinal;
+   end adafinal;
+
+   type No_Param_Proc is access procedure;
+
+   procedure adainit is
+      Main_Priority : Integer;
+      pragma Import (C, Main_Priority, "__gl_main_priority");
+      Time_Slice_Value : Integer;
+      pragma Import (C, Time_Slice_Value, "__gl_time_slice_val");
+      WC_Encoding : Character;
+      pragma Import (C, WC_Encoding, "__gl_wc_encoding");
+      Locking_Policy : Character;
+      pragma Import (C, Locking_Policy, "__gl_locking_policy");
+      Queuing_Policy : Character;
+      pragma Import (C, Queuing_Policy, "__gl_queuing_policy");
+      Task_Dispatching_Policy : Character;
+      pragma Import (C, Task_Dispatching_Policy, "__gl_task_dispatching_policy");
+      Priority_Specific_Dispatching : System.Address;
+      pragma Import (C, Priority_Specific_Dispatching, "__gl_priority_specific_dispatching");
+      Num_Specific_Dispatching : Integer;
+      pragma Import (C, Num_Specific_Dispatching, "__gl_num_specific_dispatching");
+      Main_CPU : Integer;
+      pragma Import (C, Main_CPU, "__gl_main_cpu");
+      Interrupt_States : System.Address;
+      pragma Import (C, Interrupt_States, "__gl_interrupt_states");
+      Num_Interrupt_States : Integer;
+      pragma Import (C, Num_Interrupt_States, "__gl_num_interrupt_states");
+      Unreserve_All_Interrupts : Integer;
+      pragma Import (C, Unreserve_All_Interrupts, "__gl_unreserve_all_interrupts");
+      Detect_Blocking : Integer;
+      pragma Import (C, Detect_Blocking, "__gl_detect_blocking");
+      Default_Stack_Size : Integer;
+      pragma Import (C, Default_Stack_Size, "__gl_default_stack_size");
+      Leap_Seconds_Support : Integer;
+      pragma Import (C, Leap_Seconds_Support, "__gl_leap_seconds_support");
+      Bind_Env_Addr : System.Address;
+      pragma Import (C, Bind_Env_Addr, "__gl_bind_env_addr");
+
+      procedure Runtime_Initialize (Install_Handler : Integer);
+      pragma Import (C, Runtime_Initialize, "__gnat_runtime_initialize");
+
+      Finalize_Library_Objects : No_Param_Proc;
+      pragma Import (C, Finalize_Library_Objects, "__gnat_finalize_library_objects");
+   begin
+      if Is_Elaborated then
+         return;
+      end if;
+      Is_Elaborated := True;
+      Main_Priority := -1;
+      Time_Slice_Value := -1;
+      WC_Encoding := 'b';
+      Locking_Policy := ' ';
+      Queuing_Policy := ' ';
+      Task_Dispatching_Policy := ' ';
+      Priority_Specific_Dispatching :=
+        Local_Priority_Specific_Dispatching'Address;
+      Num_Specific_Dispatching := 0;
+      Main_CPU := -1;
+      Interrupt_States := Local_Interrupt_States'Address;
+      Num_Interrupt_States := 0;
+      Unreserve_All_Interrupts := 0;
+      Detect_Blocking := 0;
+      Default_Stack_Size := -1;
+      Leap_Seconds_Support := 0;
+
+      Runtime_Initialize (1);
+
+      Finalize_Library_Objects := null;
+
+      System.Soft_Links'Elab_Spec;
+      System.Exception_Table'Elab_Body;
+      E26 := E26 + 1;
+      System.Exceptions'Elab_Spec;
+      E28 := E28 + 1;
+      System.Soft_Links'Elab_Body;
+      E16 := E16 + 1;
+      System.Secondary_Stack'Elab_Body;
+      E20 := E20 + 1;
+   end adainit;
+
+   procedure Ada_Main_Program;
+   pragma Import (Ada, Ada_Main_Program, "_ada_add_int");
+
+   function main
+     (argc : Integer;
+      argv : System.Address;
+      envp : System.Address)
+      return Integer
+   is
+      procedure Initialize (Addr : System.Address);
+      pragma Import (C, Initialize, "__gnat_initialize");
+
+      procedure Finalize;
+      pragma Import (C, Finalize, "__gnat_finalize");
+      SEH : aliased array (1 .. 2) of Integer;
+
+      Ensure_Reference : aliased System.Address := Ada_Main_Program_Name'Address;
+      pragma Volatile (Ensure_Reference);
+
+   begin
+      gnat_argc := argc;
+      gnat_argv := argv;
+      gnat_envp := envp;
+
+      Initialize (SEH'Address);
+      adainit;
+      Ada_Main_Program;
+      adafinal;
+      Finalize;
+      return (gnat_exit_status);
+   end;
+
+--  BEGIN Object file/option list
+   --   ./add_int.o
+   --   -L./
+   --   -L/usr/lib/gcc/x86_64-linux-gnu/7/adalib/
+   --   -shared
+   --   -lgnat-7
+--  END Object file/option list   
+
+end ada_main;
