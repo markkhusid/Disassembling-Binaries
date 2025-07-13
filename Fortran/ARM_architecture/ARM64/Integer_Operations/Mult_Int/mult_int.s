61b4098523ae34cb8c3bdf21eaa933d2d5b12210
Mark Khusid <markkhusid@protonmail.com>
2025-07-13 15:58:19 -0400

    recompiled from chromebook

--- Fortran/ARM_architecture/ARM64/Integer_Operations/Mult_Int/mult_int.s
+++ Fortran/ARM_architecture/ARM64/Integer_Operations/Mult_Int/mult_int.s
@@ -1,10 +1,13 @@
 	.arch armv8-a
-	.file	"mult.f08"
+	.file	"mult_int.f08"
 	.text
 	.align	2
 	.type	MAIN__, %function
 MAIN__:
+.LFB0:
+	.cfi_startproc
 	sub	sp, sp, #16
+	.cfi_def_cfa_offset 16
 	mov	w0, 2
 	str	w0, [sp, 12]
 	mov	w0, 3
@@ -15,40 +18,52 @@ MAIN__:
 	str	w0, [sp, 4]
 	nop
 	add	sp, sp, 16
+	.cfi_def_cfa_offset 0
 	ret
+	.cfi_endproc
+.LFE0:
 	.size	MAIN__, .-MAIN__
 	.align	2
 	.global	main
 	.type	main, %function
 main:
+.LFB1:
+	.cfi_startproc
 	stp	x29, x30, [sp, -32]!
-	add	x29, sp, 0
-	str	w0, [x29, 28]
-	str	x1, [x29, 16]
-	ldr	x1, [x29, 16]
-	ldr	w0, [x29, 28]
+	.cfi_def_cfa_offset 32
+	.cfi_offset 29, -32
+	.cfi_offset 30, -24
+	mov	x29, sp
+	str	w0, [sp, 28]
+	str	x1, [sp, 16]
+	ldr	x1, [sp, 16]
+	ldr	w0, [sp, 28]
 	bl	_gfortran_set_args
-	adrp	x0, options.0.2561
-	add	x0, x0, :lo12:options.0.2561
-	mov	x1, x0
+	adrp	x0, options.0.0
+	add	x1, x0, :lo12:options.0.0
 	mov	w0, 7
 	bl	_gfortran_set_options
 	bl	MAIN__
 	mov	w0, 0
 	ldp	x29, x30, [sp], 32
+	.cfi_restore 30
+	.cfi_restore 29
+	.cfi_def_cfa_offset 0
 	ret
+	.cfi_endproc
+.LFE1:
 	.size	main, .-main
 	.section	.rodata
 	.align	3
-	.type	options.0.2561, %object
-	.size	options.0.2561, 28
-options.0.2561:
-	.word	68
-	.word	1023
+	.type	options.0.0, %object
+	.size	options.0.0, 28
+options.0.0:
+	.word	2116
+	.word	4095
 	.word	0
 	.word	1
 	.word	1
 	.word	0
 	.word	31
-	.ident	"GCC: (Ubuntu/Linaro 7.3.0-27ubuntu1~18.04) 7.3.0"
+	.ident	"GCC: (Debian 10.2.1-6) 10.2.1 20210110"
 	.section	.note.GNU-stack,"",@progbits
